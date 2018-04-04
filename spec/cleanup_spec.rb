ebdc29148a5163f7668cf97d246eba0d03da7c44
Nikita Fedyashev <nfedyashev@gmail.com>
2018-04-04 08:48:03 +0300

    Use more appropriate matcher for easier diffing between actual/expected results

--- spec/cleanup_spec.rb
+++ spec/cleanup_spec.rb
@@ -16,22 +16,22 @@ RSpec.describe Cleanup do
 
   let(:root_path) { "tmp/spec/fixture" }
 
-  subject { -> { @cleanup.explore } }
+  subject { @cleanup.explore ; @cleanup.execute_commands }
 
-  context 'Downloads/' do
-    before(:example) do
-      touch "Downloads/file.torrent"
-      touch "Downloads/index.htm"
-      touch "Downloads/.gitkeep"
-      touch "Downloads/steam_latest.deb"
-      touch "Downloads/manual.doc"
-      touch "Downloads/dubplate-fm.mp3"
-    end
-
-    it { is_expected.to change{ @cleanup.execute_commands }.to(["mv tmp/spec/fixture/Downloads/manual.doc tmp/spec/fixture/Documents/Unsorted",
-                                                                "rm tmp/spec/fixture/Downloads/index.htm",
-                                                                "rm tmp/spec/fixture/Downloads/steam_latest.deb",
-                                                                "rm tmp/spec/fixture/Downloads/file.torrent"]) }
+  before(:example) do
+    touch "Downloads/file.torrent"
+    touch "Downloads/index.htm"
+    touch "Downloads/.gitkeep"
+    touch "Downloads/steam_latest.deb"
+    touch "Downloads/manual.doc"
+    touch "Desktop/Scan1.PDF"
+    touch "Downloads/dubplate-fm.mp3"
   end
+
+  it { is_expected.to contain_exactly("mv tmp/spec/fixture/Downloads/manual.doc tmp/spec/fixture/Documents/Unsorted",
+                                                       "rm tmp/spec/fixture/Downloads/index.htm",
+                                                       "rm tmp/spec/fixture/Downloads/steam_latest.deb",
+                                                       "rm tmp/spec/fixture/Downloads/file.torrent",
+                                                       "rm tmp/spec/fixture/Downloads/dubplate-fm.mp3")}
 end
 
